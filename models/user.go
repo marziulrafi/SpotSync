71157ba58bf633f9822584cb8c5ea93ff3fb5b10
K M Marziul Karim <marziulrafi@gmail.com>
2026-06-27 22:46:59 +0600

    Setup

---
+++ models/user.go
@@ -0,0 +1,13 @@
+package models
+
+import "time"
+
+type User struct {
+	ID        uint      `gorm:"primaryKey;autoIncrement" json:"id"`
+	Name      string    `gorm:"not null" json:"name"`
+	Email     string    `gorm:"uniqueIndex;not null" json:"email"`
+	Password  string    `gorm:"not null" json:"-"`
+	Role      string    `gorm:"default:driver;not null" json:"role"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
